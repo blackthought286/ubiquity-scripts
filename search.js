84e8c5786a3e584c06f11f4708b4c2461c292bda
Everett <blackthought286@gmail.com>
2008-08-28 19:20:00 -0400

    added extra attributes to search

--- search.js
+++ search.js
@@ -1,15 +1,17 @@
 CmdUtils.CreateCommand({
 	name: "scribd", 
+	icon: "http://scribd.com/favicon.ico",
+	author: {name: "Everett Bryant", email: "blackthought286@gmail.com"},
 	takes: {command: noun_arb_text},
-	description: "Searches Scribd.com",
+	description: "Searches Scribd.com for papers and articles",
 	preview: function(pblock, cmd){
 		pblock.innerHTML = "You are searching for: " + cmd.text;
 	},
 	execute: function(cmd){
 		var search = cmd.text;
-                var newCmd = search.replace(/\s+/g, "+");
+        var newCmd = search.replace(/\s+/g, "+");
 		var url = "http://www.scribd.com/search/search?query=" + newCmd + "&x=0&y=0";
 		//openUrl(url);
-                Utils.openUrlInBrowser(url);
+        Utils.openUrlInBrowser(url);
 	}
 });
